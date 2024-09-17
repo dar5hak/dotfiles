d92e67802db250ba6ca3d587965885fab4fb2045
Darshak Parikh <darshak@proton.me>
2024-09-17 11:47:58 +0530

    Update aliases

--- aliases.zsh
+++ aliases.zsh
@@ -11,13 +11,15 @@ alias zj="zellij"
 alias za="zellij attach"
 alias zs="zellij --session"
 alias zls="zellij list-sessions"
+alias zide="zellij --layout=ide"
 
 # misc
 alias python="python3"
 alias pip="pip3"
+alias lg="lazygit"
 
 function clone() {
-	git clone https://github.com/dar5hak/$1.git && cd $1
+	git clone https://github.com/dar5hak/$1.git && cd $1 && git config user.email "darshak@proton.me"
 }
 
 function syncup() {
